3cd873d2666f6f6593f00a657695c3ec0139269d
Amir Mollaee <amir.mollaee1369@gmail.com>
2025-12-19 22:36:57 +0330

    feature: add git ignore

--- src/IELTSExaminer.Domain/obj/Debug/net10.0/IELTSExaminer.Domain.AssemblyInfo.cs
+++ src/IELTSExaminer.Domain/obj/Debug/net10.0/IELTSExaminer.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IELTSExaminer.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7049e31ba92c42cfe712ac07f3a29de085f114f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a534baff401cfe235cc0833845e0778d210a0d24")]
 [assembly: System.Reflection.AssemblyProductAttribute("IELTSExaminer.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IELTSExaminer.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
